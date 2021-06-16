7b9022d98e3a9cda526ab0a865775315b747c0ef
Barsik1976 <shlippenbah@mail.ru>
2021-06-16 09:57:04 +0300

    Удаление лишней функции

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-// Нахождение случайного целого числа методом округления: Math.floor
-
-function getRandomInteger(from, to) {
-  if (from <= to) {
-    let getRandom = Math.floor(Math.random() * (to - from)) + from;
-    console.log(getRandom);
-    console.log("верный диапазон");
-  } else {
-    console.log("неверный диапазон");
-  }
-}
-
-getRandomInteger(10, 20);
-
 // Нахождение случайного нецелого числа, убрав Math.floor
 
 function getRandomInteger(from, to) {
